548eb60f997acd856b78eff3242186c8002a84b6
Sarah Xu <sar.chu.xu@gmail.com>
2024-07-12 11:58:24 -0700

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Sarah Xu on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
